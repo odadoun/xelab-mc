af5b3fc6a27c1133708e8529c21938cbe90fb23d
e-masson <masson@lal.in2p3.fr>
2023-05-17 14:18:42 +0200

    Testing cpp-linter

--- src/XelabConstruction.cc
+++ src/XelabConstruction.cc
@@ -13,6 +13,10 @@ XelabDetectorConstruction::~XelabDetectorConstruction()
 // Definitions from XENON class Xenon1tMaterials
 void XelabDetectorConstruction::DefineMaterials()
 {
+  const test abcd = 58. ;
+  for(int i = 0; i < 5; i ++){
+  G4cout << i << G4endl;}
+
   G4NistManager *pNistManager = G4NistManager::Instance();
 
   const G4int nEntries = 32;
